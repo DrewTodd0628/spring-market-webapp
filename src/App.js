a634753fc970374aa77a36da3bc4804b6e5a4163
Drew Todd <dretodd0628@email.com>
2022-10-21 14:24:19 -0400

    Added auth

--- src/App.js
+++ src/App.js
@@ -6,18 +6,41 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import ShoppingCart from './components/ShoppingCart';
 import CheckOut from './components/CheckOut';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+
+function App({ signOut }) {
   return (
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index path="/" element={<Shop/>}/>
-        <Route index path="/shop" element={<Shop/>}/>
-        <Route index path="/shopping-cart" element={<ShoppingCart/>}/>
-        <Route index path="/checkout" element={<CheckOut/>}/>
-      </Route>
-    </Routes>
+    <View className="App">
+      <Card>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//         <Route index path="/" element={<Shop/>}/>
+//         <Route index path="/shop" element={<Shop/>}/>
+//         <Route index path="/shopping-cart" element={<ShoppingCart/>}/>
+//         <Route index path="/checkout" element={<CheckOut/>}/>
+//       </Route>
+//     </Routes>
+//   );
+// }
+
+// export default App;
